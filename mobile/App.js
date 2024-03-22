7ed693a646d77b33a332bbf00399d2f1e2b5fa29
Erdal Karaer <karaer.erd@gmail.com>
2024-03-22 10:28:16 +0100

    Complete refont of structure project

---
+++ mobile/App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import "./global.css";
+
+export default function App() {
+  return (
+    <View>
+      <Text className="text-red-500">
+        Open up App.js to start working on your app!
+      </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,14 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.jsx"],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./App.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
+};
