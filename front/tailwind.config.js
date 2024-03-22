238ea65416b964ff8d7ddaeeee9518ac6247db4c
Erdal Karaer <karaer.erd@gmail.com>
2024-03-22 10:08:43 +0100

    Refont of front folder

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.jsx"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       backgroundImage: {
@@ -11,4 +15,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
