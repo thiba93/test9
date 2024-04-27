d5b66823c1d2bda2aad195a597950a9f44b60474
Romain VIGNARD <romain78780@gmail.com>
2024-04-27 12:40:25 +0200

    route to sign-in

--- web/src/pages/sign-up.jsx
+++ web/src/pages/sign-up.jsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router"
 import { emailValidator, usernameValidator, passwordValidator } from "@/utils/validators"
 import Form from "@/components/ui/Form"
 import FormField from "@/components/ui/FormField"
@@ -17,51 +18,56 @@ const validationSchema = object({
   password: passwordValidator.required().label("Password"),
   username: usernameValidator.required().label("Username"), 
 })
-const onSubmit = async (values, { setSubmitting, resetForm, setErrors }) => {
-  try {
-    await axios.post("/api/users", values)
-    resetForm() 
-  } catch (error) {
-    setErrors({ submit: error.response.data.error })
+const SignUpPage = () => {
+  const router = useRouter()
+  const onSubmit = async (values, { setSubmitting, resetForm, setErrors }) => {
+    try {
+      await axios.post("/api/users", values)
+      resetForm()
+      router.push("/sign-in")
+    } catch (error) {
+      setErrors({ submit: error.response.data.error })
+    }
+
+    setSubmitting(false)
   }
 
-  setSubmitting(false)
+  return (
+    <Layout>
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "100%" }}>
+        <h1 style={{ marginTop: "20px", textAlign: "center", fontWeight: "bold", fontSize: "24px" }}>Inscription</h1>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
+        >
+          {formikProps => (
+            <Form>
+              <FormField
+                name="email"
+                type="email"
+                placeholder="Enter your e-mail"
+                label="E-mail"
+              />
+              <FormField
+                name="password"
+                type="password"
+                placeholder="Enter your password"
+                label="Password"
+              />
+              <FormField
+                name="username"
+                type="text"
+                placeholder="Choose a username"
+                label="Username"
+              />
+              <SubmitButton disabled={formikProps.isSubmitting}>Sign Up</SubmitButton>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </Layout>
+  )
 }
-const SignUpPage = () => (
-  <Layout>
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", width: "100%" }}>
-      <h1 style={{ marginTop: "20px", textAlign: "center", fontWeight: "bold", fontSize: "24px" }}>Inscription</h1>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={onSubmit}
-      >
-        {formikProps => (
-          <Form>
-            <FormField
-              name="email"
-              type="email"
-              placeholder="Enter your e-mail"
-              label="E-mail"
-            />
-            <FormField
-              name="password"
-              type="password"
-              placeholder="Enter your password"
-              label="Password"
-            />
-            <FormField
-              name="username"
-              type="text"
-              placeholder="Choose a username"
-              label="Username"
-            />
-            <SubmitButton disabled={formikProps.isSubmitting}>Sign Up</SubmitButton>
-          </Form>
-        )}
-      </Formik>
-    </div>
-  </Layout>
-)
 
 export default SignUpPage
