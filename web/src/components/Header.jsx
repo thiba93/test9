bfdbb20a596e16e18de6803d25805a1266961c28
Romain VIGNARD <romain78780@gmail.com>
2024-06-06 21:59:40 +0200

    burger_menu

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -1,28 +1,11 @@
-/* eslint-disable max-lines-per-function */
-import { useRouter } from "next/router"
 import Link from "next/link"
 import {
-  Bars3Icon,
   MagnifyingGlassIcon,
   ShoppingCartIcon,
 } from "@heroicons/react/24/outline"
-import { isAuthenticated, removeToken } from "../utils/auth"
-import { useEffect, useState } from "react"
+import BurgerMenu from "./ui/BurgerMenu"
 
-const Header = () => {
-  const router = useRouter()
-  const [auth, setAuth] = useState(false)
-
-  useEffect(() => {
-    setAuth(isAuthenticated())
-  }, [])
-
-  const handleLogout = () => {
-    removeToken()
-    router.push("/sign-up")
-  }
-
-  return (
+const Header = () => (
     <header className="bg-primary-blue p-4 flex justify-between items-center">
       <Link href="/" legacyBehavior>
         <a className="font-semibold">AIRNEIS</a>
@@ -30,48 +13,24 @@ const Header = () => {
 
       <ul className="flex gap-4 items-center">
         <li>
-          <Link href="/search" legacyBehavior>
+        <Link href="/search" legacyBehavior>
             <a>
               <MagnifyingGlassIcon className="h-6 w-6" />
             </a>
           </Link>
         </li>
         <li>
-          <Link href="/cart" legacyBehavior>
+        <Link href="/cart" legacyBehavior>
             <a>
               <ShoppingCartIcon className="h-6 w-6" />
             </a>
           </Link>
         </li>
         <li>
-          <Bars3Icon className="h-6 w-6" />
+          <BurgerMenu />
         </li>
-        {auth ? (
-          <>
-            <li>
-              <button
-                onClick={handleLogout}
-                className="text-white font-semibold"
-              >
-                Déconnexion
-              </button>
-            </li>
-            <li>
-              <Link href="/backoffice" legacyBehavior>
-                <a className="font-semibold">backoffice</a>
-              </Link>
-            </li>
-          </>
-        ) : (
-          <li>
-            <Link href="/sign-in" legacyBehavior>
-              <a className="font-semibold">Se connecter</a>
-            </Link>
-          </li>
-        )}
       </ul>
     </header>
   )
-}
 
 export default Header
