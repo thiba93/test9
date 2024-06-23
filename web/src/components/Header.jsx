042b850f0a209c668b795eef72acd6c9291bb665
thiba93 <thibaultsene4@gmail.com>
2024-06-23 21:31:06 +0200

    history airneis

--- web/src/components/Header.jsx
+++ web/src/components/Header.jsx
@@ -6,31 +6,34 @@ import {
 import BurgerMenu from "./ui/BurgerMenu"
 
 const Header = () => (
-    <header className="bg-primary-blue p-4 flex justify-between items-center">
-      <Link href="/" legacyBehavior>
-        <a className="font-semibold">AIRNEIS</a>
-      </Link>
+  <header className="bg-primary-blue p-4 flex justify-between items-center">
+    <Link href="/" legacyBehavior>
+      <a className="font-semibold">AIRNEIS</a>
+    </Link>
 
-      <ul className="flex gap-4 items-center">
-        <li>
+    <ul className="flex gap-4 items-center">
+      <li>
+        <Link href="/history">Histoire</Link>
+      </li>
+      <li>
         <Link href="/search" legacyBehavior>
-            <a>
-              <MagnifyingGlassIcon className="h-6 w-6" />
-            </a>
-          </Link>
-        </li>
-        <li>
+          <a>
+            <MagnifyingGlassIcon className="h-6 w-6" />
+          </a>
+        </Link>
+      </li>
+      <li>
         <Link href="/cart" legacyBehavior>
-            <a>
-              <ShoppingCartIcon className="h-6 w-6" />
-            </a>
-          </Link>
-        </li>
-        <li>
-          <BurgerMenu />
-        </li>
-      </ul>
-    </header>
-  )
+          <a>
+            <ShoppingCartIcon className="h-6 w-6" />
+          </a>
+        </Link>
+      </li>
+      <li>
+        <BurgerMenu />
+      </li>
+    </ul>
+  </header>
+)
 
 export default Header
